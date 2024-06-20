b5a8ac670f1f0e51b7fb361242f110689838eddb
Tomas Brogan <tomasbrogan02@gmail.com>
2024-06-20 14:10:01 +1000

    modified:   README.md 	modified:   app.py 	modified:   data_files/characters.csv 	modified:   data_files/sessions.csv

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-A website made to easily train one's knowledge of the readings and tones of any hanzi you wish with a KameSame-inspired interface. It does this by taking a list of hanzi you want to practice, then sampling a dictionary of either HSK words or dictionary words to show you that you will give the pinyin for. It then checks each character against your entry and update your skill level with that character according to whether you got its reading correct or not. If you got it wrong, words with that character will appear more often, and vice versa.
+A website made to easily train one's knowledge of the readings and tones of any hanzi you wish with a KameSame-inspired interface.
+
+It does this by taking a list of hanzi you want to practice, then sampling a dictionary of either HSK words or dictionary words to show you that you will give the pinyin for. It then checks each character against your entry and update your skill level with that character according to whether you got its reading correct or not. If you got it wrong, words with that character will appear more often, and vice versa.
+
+Not actually a website atm, you'll have to run this on your own device.
--- app.py
+++ app.py
@@ -20,27 +20,15 @@ def load_words():
         reader = csv.reader(file)
         return {row[0] : row[1].rstrip().split('|') for row in reader if all(char in allowed_chars_set for char in row[0])}
 
-sessions = load_sessions()
-sessions.append((str(len(sessions)), '0', '0'))
-
-character_dict = load_characters()
-allowed_chars_set = set(character_dict.keys())
-print(f'Loaded {len(allowed_chars_set)} characters.')
-
-word_dict = load_words()
-words = list(word_dict.keys())
-print(f'Loaded {len(words)} words.')
-
-history = []
-correct_num = 0
-total_num = 0
-
 def calculate_weights():
+    # gives each character a weight exponentially proportional to the net number of incorrect answers
     char_weights = {}
     for char in allowed_chars_set:
         correct_num = character_dict[char].count('1')
         incorrect_num = character_dict[char].count('0')
         char_weights[char] = 1.3 ** (incorrect_num - correct_num)
+    
+    # takes geometric mean of each character's weight in each word
     weights = []
     for word in words:
         net = 1
@@ -48,7 +36,47 @@ def calculate_weights():
             net *= char_weights[char]
         net = net ** (1/len(word))
         weights.append(net)
+        
+    # normalises weights
+    mean = sum(weights)/len(weights) if len(weights) != 0 else 1.0
+    weights = [weight / mean for weight in weights]
+    
     return weights
+
+def start_session():
+    global sessions
+    
+    global character_dict
+    global allowed_chars_set
+    global word_dict
+    global words
+    
+    global history
+    global correct_num
+    global total_num
+    
+    sessions = load_sessions()
+    sessions.append((str(len(sessions)), '0', '0'))
+
+    character_dict = load_characters()
+    allowed_chars_set = set(character_dict.keys())
+    print(f'Loaded {len(allowed_chars_set)} characters.')
+
+    word_dict = load_words()
+    words = list(word_dict.keys())
+    print(f'Loaded {len(words)} words.')
+
+    history = []
+    correct_num = 0
+    total_num = 0
+    
+
+def weights_analysis():
+    weights = calculate_weights()
+    print(f'Min weight ({words[weights.index(min(weights))]}): {min(weights)}')
+    print(f'Max weight ({words[weights.index(max(weights))]}): {max(weights)}')
+    print(f'Standard deviation: {(sum([weight**2 for weight in weights])/len(weights) - (sum(weights)/len(weights))**2)**0.5 if len(weights) != 0 else 1.0}')
+    
 
 @app.route('/')
 def home():
@@ -56,19 +84,14 @@ def home():
 
 @app.route('/reviews')
 def reviews():
+    start_session()
+    weights_analysis()
     return render_template('review.html', return_url='/')
 
 @app.route('/get_word', methods=['GET'])
 def get_word():
     weights = calculate_weights()
     word = random.choices(words, weights=weights, k=1)[0]
-    '''
-    print(f'Weight ({word}): {weights[words.index(word)]}')
-    print(f'Min weight ({words[weights.index(min(weights))]}): {min(weights)}')
-    print(f'Max weight ({words[weights.index(max(weights))]}): {max(weights)}')
-    print(f'Mean weight: {sum(weights)/len(weights) if len(weights) != 0 else 1.0}')
-    print(f'Standard deviation: {(sum([weight**2 for weight in weights])/len(weights) - (sum(weights)/len(weights))**2)**0.5 if len(weights) != 0 else 1.0}')
-    '''
     return jsonify(word = word)
 
 @app.route('/check_answer', methods=['POST'])
@@ -85,13 +108,17 @@ def check_answer():
     
     print(user_input, current_word, correct_answers)
     
+    # instant fail if wrong number of characters
     if len(user_input.split(' ')) == len(current_word):
+        # checks each character of input for compatibility with answers
         for char in set(current_word):
             total_num += 1
             compatible = True
+            # checks each instance of character for compatibility in position
             for index in [i for i in range(len(current_word)) if current_word[i] == char]:
                 i_compatible = False
                 
+                # if there's an answer where the reading was valid, compatible for given index
                 for answer in correct_answers:
                     if answer.split(' ')[index] in (user_input.split(' ')[index], user_input.split(' ')[index] + '5'):
                         i_compatible = True
@@ -100,13 +127,15 @@ def check_answer():
                 if not i_compatible:
                     compatible = False
                     break                    
-                    
+            
+            # only correct if every instance of character is compatible
             if compatible:
                 correct_num += 1
                 all_wrong = False
             else:
                 all_correct = False
-                
+            
+            # updates to character history
             character_dict[char] += '1' if compatible else '0'
             
     else:
@@ -125,6 +154,7 @@ def check_answer():
 def undo():
     global correct_num
     global total_num
+    
     if len(history) != 0:
         prev_word = history.pop()
         
@@ -142,6 +172,7 @@ def undo():
 def get_accuracy():
     global correct_num
     global total_num
+    
     if total_num != 0:
         return jsonify(accuracy=round(100 * correct_num/total_num), correct=correct_num, incorrect=total_num-correct_num)
     else:
